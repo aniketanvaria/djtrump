3d65d92c20789c285ce999c06fea3fe40c7bc31c
aniket8892 <aniket@sp-assurance.com>
2020-05-08 02:21:23 +0530

    change2

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-ssh root@3.6.55.22 <<EOF
+ssh ubuntu@3.6.55.22 <<EOF
   cd djtrump
   git pull origin master
+  virtualenv venv
+  source venv/bin/activate
   ./manage.py makemigrations
   ./manage.py migrate
   ./manage.py runserver
