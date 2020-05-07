84969011e1ff3a3cc35d46fee5327425bb285e56
aniket8892 <aniket@sp-assurance.com>
2020-05-08 01:44:45 +0530

    change

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-ssh root@104.236.57.112 <<EOF
+ssh root@3.6.55.22 <<EOF
   cd djtrump
   git pull
-  source /opt/envs/djtrump/bin/activate
+  virtualenv venv
+  source venv/bin/activate
   pip install -r requirements.txt
+  ./manage.py makemigrations
   ./manage.py migrate
-  sudo supervisorctl restart djtrump
   exit
 EOF
